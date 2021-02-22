22f4f991fa7ae8f8b8d39b81a71888d1eba7375f
Anuj Singh <singhanuj620@gmail.com>
2021-02-22 20:45:58 +0530

    added hero section

--- src/Components/Layouts/Homepage.js
+++ src/Components/Layouts/Homepage.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import BookIcon from "@material-ui/icons/Book";
 import MainHeader from "../Homepage/MainHeader";
 import Trending from "../Homepage/Trending";
 import "../../Css/Layout/Homepage.css";
@@ -7,8 +7,21 @@ import "../../Css/Layout/Homepage.css";
 const Homepage = () => {
   return (
     <div className="Homepage">
-      <MainHeader />
+      <div className="poster_div">
+        <div className="poster_metadata">
+          <div className="poster_title">WEB PAPER</div>
+          <div className="poster_description">
+            The New Gen Newspaper <BookIcon style={{ fontSize: 40 }} />
+          </div>
+        </div>
+        <img
+          src="Assets/Images/Homepage/main_header_side_poster.svg"
+          alt="Web Paper"
+          className="poster_svg"
+        />
+      </div>
       <Trending />
+      <MainHeader />
     </div>
   );
 };
